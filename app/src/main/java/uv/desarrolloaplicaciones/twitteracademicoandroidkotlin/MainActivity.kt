63c5b1b54b0f1f9fafb26d9089bc2c7c6576a26c
Asis <asis.reyes@hotmail.com>
2021-11-17 17:00:58 -0600

    Splash de la app.

--- app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/MainActivity.kt
+++ app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/MainActivity.kt
@@ -2,10 +2,23 @@ package uv.desarrolloaplicaciones.twitteracademicoandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.media.Image
+import android.os.Handler
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imgSplash = findViewById<ImageView>(R.id.imgSplash)
+        Glide.with(this)
+            .load("https://i.imgur.com/Q1mGsD3.png")
+            .placeholder(R.drawable.ic_launcher_background)
+            .error(R.drawable.ic_launcher_background)
+            .into(imgSplash)
     }
 }
---
+++ app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/SplashScreenActivity.kt
@@ -0,0 +1,16 @@
+package uv.desarrolloaplicaciones.twitteracademicoandroidkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+
+
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        finish()
+    }
+}
