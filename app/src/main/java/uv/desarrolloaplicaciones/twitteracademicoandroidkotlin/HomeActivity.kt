9587fbd1942591419603166ff60ac6f76a6a647d
Dan <olveradan@hotmail.com>
2021-11-18 16:17:28 -0600

    Ventana principal de Twitter academico

---
+++ app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/HomeActivity.kt
@@ -0,0 +1,13 @@
+package uv.desarrolloaplicaciones.twitteracademicoandroidkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.drawee.backends.pipeline.Fresco
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        Fresco.initialize(this)
+    }
+}
