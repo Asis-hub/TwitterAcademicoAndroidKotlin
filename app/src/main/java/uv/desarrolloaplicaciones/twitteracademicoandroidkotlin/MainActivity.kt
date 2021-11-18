06b529cfeaea0d3ac669d16ab3ca881f2ff36f55
Asis <asis.reyes@hotmail.com>
2021-11-18 16:12:35 -0600

    Layout de ventana de login creado.

--- app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/MainActivity.kt
+++ app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.content.Intent
 import android.media.Image
 import android.os.Handler
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 
 
@@ -14,7 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnCrearcuenta = findViewById<Button>(R.id.btnCrearCuenta)
+        val tv_login = findViewById<TextView>(R.id.tv_login)
 
+        tv_login.setOnClickListener {
+            val intento1 = Intent(this, LoginActivity::class.java)
+            startActivity(intento1)
+        }
 
     }
 }
