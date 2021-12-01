231c84394fa637698c71c132687a1935df5c62fd
Dan <olveradan@hotmail.com>
2021-11-30 19:14:11 -0600

    Se agrego un metodo para obtener una instancia de Retrofit

--- app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/CrearCuenta.kt
+++ app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/CrearCuenta.kt
@@ -2,10 +2,24 @@ package uv.desarrolloaplicaciones.twitteracademicoandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import uv.desarrolloaplicaciones.twitteracademicoandroidkotlin.databinding.ActivityMainBinding
 
 class CrearCuenta : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_crear_cuenta)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://localhost:8000/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
