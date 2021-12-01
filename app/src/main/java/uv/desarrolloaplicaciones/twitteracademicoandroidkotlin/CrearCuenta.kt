b9c1168a6b20147b5fc681036b475c29b8403f45
Dan <olveradan@hotmail.com>
2021-11-30 19:55:57 -0600

    Método que consume el API para registrar un usuario

--- app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/CrearCuenta.kt
+++ app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/CrearCuenta.kt
@@ -2,8 +2,13 @@ package uv.desarrolloaplicaciones.twitteracademicoandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import uv.desarrolloaplicaciones.twitteracademicoandroidkotlin.Api.APIService
 import uv.desarrolloaplicaciones.twitteracademicoandroidkotlin.databinding.ActivityMainBinding
 
 class CrearCuenta : AppCompatActivity() {
@@ -18,8 +23,47 @@ class CrearCuenta : AppCompatActivity() {
 
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("http://localhost:8000/")
+            .baseUrl("http://localhost:8000/usuarios/?")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    private fun registrarUsuario(
+        nombre: String,
+        apellidoPaterno: String,
+        apellidoMaterno: String,
+        fechaNacimiento: String,
+        email: String,
+        nombreUsuario: String,
+        password: String,
+        idTipoUsuario: Int
+    ) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java)
+                .registrarUsuario(
+                    "Nombre=$nombre&" +
+                        "ApellidoPaterno=$apellidoPaterno&" +
+                        "ApellidoMaterno=$apellidoMaterno&" +
+                        "FechaNacimiento=$fechaNacimiento&" +
+                        "Email=$email" +
+                        "NombreUsuario=$nombreUsuario&" +
+                        "Password=$password&" +
+                        "idTipoUsuario=$idTipoUsuario"
+                )
+            val response = call.body()
+            if (call.isSuccessful) {
+                mostrarExito()
+            } else {
+                mostrarError()
+            }
+        }
+    }
+
+    private fun mostrarError() {
+        Toast.makeText(this,"Error al crear la cuenta",Toast.LENGTH_SHORT)
+    }
+
+    private fun mostrarExito() {
+        Toast.makeText(this,"¡Cuenta creada!",Toast.LENGTH_SHORT)
     }
 }
