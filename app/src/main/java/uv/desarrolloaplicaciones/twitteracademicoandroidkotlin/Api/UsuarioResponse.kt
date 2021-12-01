8967b9785d1004e0ef412e6a76b08785e09dfae1
Dan <olveradan@hotmail.com>
2021-11-30 19:04:41 -0600

    Data class para el usuario

---
+++ app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/Api/UsuarioResponse.kt
@@ -0,0 +1,23 @@
+package uv.desarrolloaplicaciones.twitteracademicoandroidkotlin.Api
+
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
+data class UsuarioResponse(
+    @SerializedName("Nombre")
+    var nombre: String,
+    @SerializedName("ApellidoPaterno")
+    var apellidoPaterno: String,
+    @SerializedName("ApellidoMaterno")
+    var apellidoMaterno: String,
+    @SerializedName("FechaNacimiento")
+    var fechaNacimiento: String,
+    @SerializedName("Email")
+    var email: String,
+    @SerializedName("NombreUsuario")
+    var nombreUsuario: String,
+    @SerializedName("Password")
+    var password: String,
+    @SerializedName("IdTipoUsuario")
+    var idTipoUsuario: Int
+)
