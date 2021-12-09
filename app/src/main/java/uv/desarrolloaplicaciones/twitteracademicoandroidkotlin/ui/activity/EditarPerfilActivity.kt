e60085b43aae73836a8828e32b419ec96ce07eb9
Emilio Alarcón <enano_2206@hotmail.com>
2021-12-08 21:42:51 -0600

    Pantalla modificar perfil
    Se agrego la pantalla de modificar perfil

---
+++ app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/ui/activity/EditarPerfilActivity.kt
@@ -0,0 +1,12 @@
+package uv.desarrolloaplicaciones.twitteracademicoandroidkotlin.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uv.desarrolloaplicaciones.twitteracademicoandroidkotlin.R
+
+class EditarPerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_perfil)
+    }
+}
