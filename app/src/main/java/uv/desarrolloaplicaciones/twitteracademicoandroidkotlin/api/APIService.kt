92a7c45c619ec5f6d44b912c635abddf9e2ac246
Dan <olveradan@hotmail.com>
2021-12-08 22:17:17 -0600

    Ventana principal
    Muestra todos los tweets de un usuario y las personas que sigue

--- app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/api/APIService.kt
+++ app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/api/APIService.kt
@@ -2,17 +2,28 @@ package uv.desarrolloaplicaciones.twitteracademicoandroidkotlin.api
 
 import okhttp3.RequestBody
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
+import uv.desarrolloaplicaciones.twitteracademicoandroidkotlin.api.datamodels.Mensaje
+import uv.desarrolloaplicaciones.twitteracademicoandroidkotlin.api.datamodels.Tweet
 import uv.desarrolloaplicaciones.twitteracademicoandroidkotlin.api.datamodels.Usuario
 
 interface APIService {
+    //USUARIOS
+    @GET("Usuario/{nombreUsuario}/{password}")
+    suspend fun logearse(@Path("nombreUsuario") nombreUsuario: String,
+                         @Path("password") password: String): Usuario
+
     @GET("Usuario")
     suspend fun getUsuarios() : List<Usuario>
 
     @Headers("Content-Type: application/json")
     @POST("Usuario")
     suspend fun registrarUsuario(@Body nuevoUsuario: RequestBody): Response<Usuario>
+
+    //TWEETS
+    @GET("Tweet")
+    suspend fun recuperarTweets(): List<Tweet>
+
+    @GET("Tweet/{id}")
+    suspend fun recuperarTweets(@Path("id") idUsuario: Int): List<Tweet>
 }
