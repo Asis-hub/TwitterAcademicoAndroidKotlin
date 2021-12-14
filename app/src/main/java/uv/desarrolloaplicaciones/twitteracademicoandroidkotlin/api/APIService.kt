041a568dde3e76b51aa2bfcf288b176041f4b456
Dan <olveradan@hotmail.com>
2021-12-14 13:06:06 -0600

    Revert "Merge branch 'master' into Dan-branch"
    This reverts commit e31120a762a6564ebe3dc470f47629f592c0ef69.

--- app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/api/APIService.kt
+++ app/src/main/java/uv/desarrolloaplicaciones/twitteracademicoandroidkotlin/api/APIService.kt
@@ -12,20 +12,20 @@ interface APIService {
     //USUARIOS
 
     @GET("Usuario/{id}")
-    suspend fun getUsuario(@Header("access-token") token: String, @Path("id")idUsuario: Int): Usuario
+    suspend fun getUsuario(@Path("id")idUsuario: Int): Usuario
 
     @POST("Login")
     suspend fun logearse(@Body usuario: RequestBody): Usuario
 
     @GET("Usuario")
-    suspend fun getUsuarios(@Header("access-token") token: String) : List<Usuario>
+    suspend fun getUsuarios() : List<Usuario>
 
     @Headers("Content-Type: application/json")
     @POST("Usuario")
     suspend fun registrarUsuario(@Body nuevoUsuario: RequestBody): Usuario
 
     @PUT("Usuario/{idUsuario}")
-    suspend fun modificarUsuario(@Header("access-token") token: String, @Path("idUsuario") idUsuario: Int,
+    suspend fun modificarUsuario(@Path("idUsuario") idUsuario: Int,
                                  @Body nuevaInfoUsuario: RequestBody): Usuario
 
 
@@ -33,11 +33,10 @@ interface APIService {
     //SEGUIDOR
 
     @POST("Seguidor")
-    suspend fun seguirUsuario(@Header("access-token") token: String, @Body seguirUsuario: RequestBody): Seguidor
+    suspend fun seguirUsuario(@Body seguirUsuario: RequestBody): Seguidor
 
     @DELETE("Seguidor/{idUsuario}/{idSeguidor}")
     suspend fun dejarSeguir(
-        @Header("access-token") token: String,
         @Path("idUsuario") idUsuarioSeguido: Int,
         @Path("idSeguidor") idUsuarioSeguidor: Int
     ): Seguidor
@@ -45,46 +44,45 @@ interface APIService {
     //Verifica si un usuario sigue a otro
     @GET("Seguidor/{idUsuarioComparacion}/{idUsuario}")
     suspend fun verificarSeguidor(
-        @Header("access-token") token: String,
         @Path("idUsuario") idUsuario: Int,
         @Path("idUsuarioComparacion") idUsuarioComparacion: Int
     ): Seguidor
 
     //Recupera una lista de los seguidores de un usuario definido
     @GET("Seguidores/{idUsuario}")
-    suspend fun recuperarSeguidores(@Header("access-token") token: String, @Path("idUsuario") idUsuario: Int): List<Seguidor>
+    suspend fun recuperarSeguidores(@Path("idUsuario") idUsuario: Int): List<Seguidor>
 
     //TWEETS
 
     @GET("Tweet")
-    suspend fun recuperarTweets(@Header("access-token") token: String): List<Tweet>
+    suspend fun recuperarTweets(): List<Tweet>
 
     @GET("Tweet/{id}")
     suspend fun recuperarTweets(@Header("access-token") token: String, @Path("id") idUsuario: Int): List<Tweet>
 
     @GET("TweetPerfil/{id}")
-    suspend fun recuperarTweetsPerfil(@Header("access-token") token: String, @Path("id") idUsuario: Int): List<Tweet>
+    suspend fun recuperarTweetsPerfil(@Path("id") idUsuario: Int): List<Tweet>
 
     @Headers("Content-Type: application/json")
     @POST("Tweet")
-    suspend fun registrarTweet(@Header("access-token") token: String, @Body nuevoTweet: RequestBody): Response<Tweet>
+    suspend fun registrarTweet(@Body nuevoTweet: RequestBody): Response<Tweet>
 
     @DELETE("Tweet/{idTweet}")
-    suspend fun eliminarTweet(@Header("access-token") token: String, @Path("idTweet") idTweet: Int): Tweet
+    suspend fun eliminarTweet(@Path("idTweet") idTweet: Int): Tweet
 
     @GET("Tweet/Content/{keyword}")
-    suspend fun buscarTweetContenido(@Header("access-token") token: String, @Path("keyword") keyword: String): List<Tweet>
+    suspend fun buscarTweetContenido(@Path("keyword") keyword: String): List<Tweet>
 
     //LIKES
   
     @GET("Likes/{idTweet}/{idUsuario}")
-    suspend fun isLiked(@Header("access-token") token: String, @Path("idTweet") idTweet: Int, @Path("idUsuario") idUsuario: Int): Like
+    suspend fun isLiked(@Path("idTweet") idTweet: Int, @Path("idUsuario") idUsuario: Int): Like
 
     @Headers("Content-Type: application/json")
     @POST("Likes")
-    suspend fun darLike(@Header("access-token") token: String, @Body nuevoLike: RequestBody): Response<Like>
+    suspend fun darLike(@Body nuevoLike: RequestBody): Response<Like>
 
     @Headers("Content-Type: application/json")
     @DELETE("Likes/{idTweet}/{idUsuario}")
-    suspend fun quitarLike(@Header("access-token") token: String, @Path("idTweet") idTweet: Int, @Path("idUsuario") idUsuario: Int): Like
+    suspend fun quitarLike(@Path("idTweet") idTweet: Int, @Path("idUsuario") idUsuario: Int): Like
 }
